9994c7439457d76d23063e5f7d5b1ec93bdaf055
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-22 15:32:43 -0500

    more fetch experiments

--- cypress/integration/fetch-spec.js
+++ cypress/integration/fetch-spec.js
@@ -16,4 +16,44 @@ describe('window.fetch', () => {
     })
     cy.visit('has-fetch.html')
   })
+
+  it('stubs fetch with alias', () => {
+    cy
+      .route({
+        url: '/users?_limit=3',
+        response: [1, 2, 3],
+        headers: {
+          'access-control-allow-origin': '*'
+        }
+      })
+      .as('users')
+    cy.visit('has-fetch.html')
+  })
+
+  it('returns mock data', () => {
+    cy
+      .route({
+        url: '/users?_limit=3',
+        response: [1, 2, 3],
+        headers: {
+          'access-control-allow-origin': '*'
+        }
+      })
+      .as('users')
+    cy.visit('has-fetch.html')
+  })
+
+  it.skip('allows inspecting response', () => {
+    cy
+      .route({
+        url: '/users?_limit=3',
+        response: [1, 2, 3],
+        headers: {
+          'access-control-allow-origin': '*'
+        }
+      })
+      .as('users')
+    cy.visit('has-fetch.html')
+    cy.get('@users').its('response.body').should('have.length', 3)
+  })
 })
